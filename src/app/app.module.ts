f33273599a57d9712f1e58e70f02a4b3c84f8fb5
Vladislav Ridnitsky <v.rudnitsky.dev@gmail.com>
2023-01-25 16:51:29 +0300

    feat: добавить основные компоненты страницы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NavComponentComponent } from './components/nav-component/nav-component.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { RootAppComponentComponent } from './components/root-app-component/root-app-component.component';
+import { ItemMainComponent } from './components/item-main/item-main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponentComponent,
-    RootAppComponentComponent
+    RootAppComponentComponent,
+    ItemMainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
