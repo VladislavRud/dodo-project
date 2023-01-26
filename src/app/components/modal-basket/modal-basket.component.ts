131239365f3c80fc519f7b26405a6bb19e4939ce
Vladislav Ridnitsky <v.rudnitsky.dev@gmail.com>
2023-01-26 13:58:21 +0300

    feat: добавить всплывающее окно корзины

---
+++ src/app/components/modal-basket/modal-basket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-basket',
+  templateUrl: './modal-basket.component.html',
+  styleUrls: ['./modal-basket.component.scss']
+})
+export class ModalBasketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
