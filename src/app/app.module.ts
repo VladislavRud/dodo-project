8302eae532f564dc9c7d2d3c2a6b8dab28481995
Vladislav Ridnitsky <v.rudnitsky.dev@gmail.com>
2023-01-27 20:40:56 +0300

    feat: добавить вкладку регистрации версия 0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponentComponent } from './components/nav-component/nav-component.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavComponentComponent} from './components/nav-component/nav-component.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { RootAppComponentComponent } from './components/root-app-component/root-app-component.component';
-import { ItemMainComponent } from './components/item-main/item-main.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ModalBasketComponent } from './components/modal-basket/modal-basket.component';
-import { PizzaComponent } from './components/pizza/pizza.component';
-import { DrinkComponent } from './components/drink/drink.component';
-import { SnacksComponent } from './components/snacks/snacks.component';
+import {RootAppComponentComponent} from './components/root-app-component/root-app-component.component';
+import {ItemMainComponent} from './components/item-main/item-main.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {ModalBasketComponent} from './components/modal-basket/modal-basket.component';
+import {PizzaComponent} from './components/pizza/pizza.component';
+import {DrinkComponent} from './components/drink/drink.component';
+import {SnacksComponent} from './components/snacks/snacks.component';
+import {AuthRegComponent} from './components/auth-reg/auth-reg.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: "home", component: RootAppComponentComponent},
+  {path: "auth", component: AuthRegComponent}
+];
 
 @NgModule({
   declarations: [
@@ -23,13 +30,15 @@ import { SnacksComponent } from './components/snacks/snacks.component';
     ModalBasketComponent,
     PizzaComponent,
     DrinkComponent,
-    SnacksComponent
+    SnacksComponent,
+    AuthRegComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/auth-reg/auth-reg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-reg',
+  templateUrl: './auth-reg.component.html',
+  styleUrls: ['./auth-reg.component.scss']
+})
+export class AuthRegComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
