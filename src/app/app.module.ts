bea18d5f126a460b311a255b76adb92382618514
Vladislav Ridnitsky <v.rudnitsky.dev@gmail.com>
2023-06-23 17:15:41 +0300

    dodo-5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DrinkComponent} from './components/drink/drink.component';
 import {SnacksComponent} from './components/snacks/snacks.component';
 import {AuthRegComponent} from './components/auth-reg/auth-reg.component';
 import {RouterModule, Routes} from "@angular/router";
+import { AuthModalRegComponent } from './components/auth-modal-reg/auth-modal-reg.component';
 
 const appRoutes: Routes = [
   {path: "home", component: RootAppComponentComponent},
@@ -31,7 +32,8 @@ const appRoutes: Routes = [
     PizzaComponent,
     DrinkComponent,
     SnacksComponent,
-    AuthRegComponent
+    AuthRegComponent,
+    AuthModalRegComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/auth-reg/auth-reg.component.ts
+++ src/app/components/auth-reg/auth-reg.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-auth-reg',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthRegComponent implements OnInit {
 
-  constructor() { }
+  authForm: FormGroup = new FormGroup<any>({
+    userName: new FormControl('', Validators.required),
+    userEmail: new FormControl('', [Validators.required, Validators.email]),
+    userPhoneNumber: new FormArray([new FormControl('', [Validators.required, Validators.pattern("[0-9+]{12}")])]),
+    userPassword: new FormControl('', Validators.pattern("[0-9]{10}")),
+  })
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  getFormsControls(): FormArray {
+    return this.authForm?.controls['userPhoneNumber'] as FormArray;
+  }
+
+  addPhone() {
+    (<FormArray>this.authForm?.controls["userPhoneNumber"]).push(new FormControl("+7", Validators.required));
+  }
+
+  submit(authForm: any) {
+    let arr = Object.entries(this.authForm.controls)
+    let donArray: any[] = []
+    arr.forEach((a:any) => {
+      console.log(a[1].value)
+      if(Array.isArray(a[1].value) && (typeof a[1].value) !== "string") {
+        donArray.push(a[1].value)
+      } else {
+        donArray.push(a[1].value?.trim())
+      }
+    })
+    return donArray
   }
 
 }
